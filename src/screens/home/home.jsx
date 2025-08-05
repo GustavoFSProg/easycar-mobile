b41a23a3d4003fd44079470771ad5ae3d88f7e87
GustavoFSProg <gustavo.prog40@gmail.com>
2025-08-05 13:17:36 -0300

    first home

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,4 +1,11 @@
-import { Image, ImageBackground, Text, View } from "react-native";
+import {
+  Alert,
+  Image,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import icons from "../../constants/icons";
 import { styles } from "./home-styles";
 
@@ -16,69 +23,76 @@ function Home() {
           source={icons.logo}
           resizeMode="none"
         />
-        <View
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            marginTop: -35,
-            width: "100vw",
-          }}
-        >
-          <Image
-            style={{ width: 160, marginBottom: -50 }}
-            source={icons.passenger}
-            resizeMode="contain"
-          />
-          <Text style={{ color: "white", fontFamily: "Roboto", fontSize: 20 }}>
-            Passageiro
-          </Text>
-          <Text
+        <TouchableOpacity onPress={() => Alert.alert("Clicou Passageiro")}>
+          <View
             style={{
-              color: "white",
-              fontFamily: "Roboto",
-              fontSize: 15,
-              opacity: 0.7,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: -35,
+              width: "100vw",
             }}
           >
-            Encontre uma carona pra você
-          </Text>
-        </View>
-        <View
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            width: "100vw",
-            marginTop: -5,
-          }}
-        >
-          <Image
-            style={{ width: 160 }}
-            source={icons.driver}
-            resizeMode="contain"
-          />
-          <Text
+            <Image
+              style={{ width: 160, marginBottom: -50 }}
+              source={icons.passenger}
+              resizeMode="contain"
+            />
+            <Text
+              style={{ color: "white", fontFamily: "Roboto", fontSize: 20 }}
+            >
+              Passageiro
+            </Text>
+            <Text
+              style={{
+                color: "white",
+                fontFamily: "Roboto",
+                fontSize: 15,
+                opacity: 0.7,
+              }}
+            >
+              Encontre uma carona pra você
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => Alert.alert("Clicou Motorista")}>
+          <View
             style={{
-              color: "white",
-              fontFamily: "Roboto",
-              marginTop: -50,
-              fontSize: 20,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100vw",
+              marginTop: -5,
             }}
           >
-            Motorista
-          </Text>
-          <Text
-            style={{
-              color: "white",
-              fontFamily: "Roboto",
-              fontSize: 15,
-              opacity: 0.7,
-            }}
-          >
-            Ofereça carona em seu Carro
-          </Text>
-        </View>
+            <Image
+              style={{ width: 160 }}
+              source={icons.driver}
+              resizeMode="contain"
+            />
+            <Text
+              style={{
+                color: "white",
+                fontFamily: "Roboto",
+                marginTop: -50,
+                fontSize: 20,
+              }}
+            >
+              Motorista
+            </Text>
+            <Text
+              style={{
+                color: "white",
+                fontFamily: "Roboto",
+                fontSize: 15,
+                opacity: 0.7,
+              }}
+            >
+              Ofereça carona em seu Carro
+            </Text>
+          </View>
+        </TouchableOpacity>
       </ImageBackground>
     </>
   );
